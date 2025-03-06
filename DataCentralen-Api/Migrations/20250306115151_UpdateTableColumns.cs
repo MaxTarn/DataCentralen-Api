025a8eaba736b1bd27896bd9f6f7c6cfcc604943
oxarious <143609934+oxarious@users.noreply.github.com>
2025-03-06 12:54:22 +0100

    Added DbInitalizer  - Updated Database and seeded data.

---
+++ DataCentralen-Api/Migrations/20250306115151_UpdateTableColumns.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataCentralen_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastEdited",
+                table: "Articles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "Articles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Posted",
+                table: "Articles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "LastEdited",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "Posted",
+                table: "Articles");
+        }
+    }
+}
